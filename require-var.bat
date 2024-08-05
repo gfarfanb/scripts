00548e166f7cf2e599c28c186cafa8ae4e91c435
Giovanni Farfán B. <gfarfanb@gmail.com>
2024-08-05 16:34:38 -0600

    Cleanup temp

--- require-var.bat
+++ require-var.bat
@@ -7,11 +7,14 @@ set __timestamp=%__timestamp: =_%
 set __timestamp=%__timestamp:.=_%
 
 set __require_var_bat="%TEMP%\require-var-%__timestamp%.bat"
+set __require_var_flag=0
 
 echo if "%%%1%%"=="" ( >> "%__require_var_bat%"
 echo echo Undefined environment variable: '%1' >> "%__require_var_bat%"
-echo cmd /k >> "%__require_var_bat%"
+echo set __require_var_flag=1 >> "%__require_var_bat%"
 echo ) >> "%__require_var_bat%"
 
 call "%__require_var_bat%"
 del "%__require_var_bat%"
+
+if "%__require_var_flag%"=="1" cmd /k

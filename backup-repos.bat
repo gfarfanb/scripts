493428a6dec0e10b0926fd5979e4dc818ede9565
Giovanni Farfán B. <gfarfanb@gmail.com>
2024-09-24 19:07:51 -0600

    Complete log

--- backup-repos.bat
+++ backup-repos.bat
@@ -5,6 +5,19 @@ call env-vars.bat
 call require-var REPOS_HOME
 call require-var REPOS_LIST_FILE
 
+goto main
+
+:usage
+echo Download the repositories backup.
+echo:
+echo Usage: %0 [^<option^>]*
+echo Option:
+echo     -h: Displays this help message
+goto back
+
+:main
+if /i "%1"=="-h" goto usage
+
 echo Backing up repos at "%REPOS_HOME%"
 
 setlocal enableDelayedExpansion
@@ -15,9 +28,16 @@ for /F "tokens=1,2" %%i in (%REPOS_LIST_FILE%) do (
     set "__repo_zip=%%i.zip"
     set "__repo_url=%%j"
 
+    echo:
+    echo Output: !__repo_zip!
+    echo URL: !__repo_url!
     curl -L --create-dirs -o !__repo_zip! !__repo_url!
 )
 
+echo [Completed]: %0
+goto back
+
 endlocal
 
+:back
 cd /d %PWD%

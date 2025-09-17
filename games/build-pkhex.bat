6367f6667d2430edcdb758d8ee946d160a9bad91
Giovanni Farfán B. <gfarfanb@gmail.com>
2025-09-17 01:52:36 -0600

    Build

---
+++ games/build-pkhex.bat
@@ -0,0 +1,27 @@
+@echo OFF
+
+call env-vars.bat
+call require-var PKHEX_HOME
+
+goto main
+
+:usage
+echo Builds PKHeX executable.
+echo ;
+echo Usage: %0 <save-home> [^<option^>]*
+echo Option:
+echo     -h: Displays this help message
+goto :eof
+
+:main
+if /i "%1"=="-h" goto usage
+
+cd "%PKHEX_HOME%"
+
+dotnet publish -r win-x64 /p:IncludeNativeLibrariesForSelfExtract=true
+goto completed
+
+:completed
+echo:
+echo [Completed]: %0
+goto :eof

794733fd4478daa4e10915d5f720f76e5137e63c
Giovanni Farfán B. <gfarfanb@gmail.com>
2025-10-09 18:24:07 -0600

    Add profile

--- games/n64-profiles.bat
+++ games/n64-profiles.bat
@@ -28,14 +28,6 @@ for /f "delims=" %%a in ('sha256sum "Banjo-Kazooie.n64"') do (
     set "_params_game_profiles[%%a]="
 )
 
-rem [Saves]
-for /f "delims=" %%a in ('sha256sum "Banjo-Kazooie (U) (V1.1) [].eep"') do (
-    set "_profile_save_files[%%a]=Banjo-Kazooie (U) (V1.1) [!].eep"
-)
-for /f "delims=" %%a in ('sha256sum "Banjo-Kazooie (U) (V1.1) [].mpk"') do (
-    set "_profile_save_files[%%a]=Banjo-Kazooie (U) (V1.1) [!].mpk"
-)
-
 
 rem /********** Banjo-Tooie **********/
 rem [Mupen64Plus Options]
@@ -44,14 +36,6 @@ for /f "delims=" %%a in ('sha256sum "Banjo-Tooie.n64"') do (
     set "_gfx_game_profiles[%%a]=mupen64plus-video-rice"
     set "_emumode_game_profiles[%%a]=Pure Interpreter"
     set "_params_game_profiles[%%a]="
-)
-
-rem [Saves]
-for /f "delims=" %%a in ('sha256sum "Banjo-Tooie (U) [].eep"') do (
-    set "_profile_save_files[%%a]=Banjo-Tooie (U) [!].eep"
-)
-for /f "delims=" %%a in ('sha256sum "Banjo-Tooie (U) [].mpk"') do (
-    set "_profile_save_files[%%a]=Banjo-Tooie (U) [!].mpk"
 )
 
 
@@ -64,7 +48,12 @@ for /f "delims=" %%a in ('sha256sum "Pokemon Stadium.n64"') do (
     set "_params_game_profiles[%%a]="
 )
 
-rem [Saves]
-for /f "delims=" %%a in ('sha256sum "Pokemon Stadium (U) (V1.0) [].fla"') do (
-    set "_profile_save_files[%%a]=Pokemon Stadium (U) (V1.0) [!].fla"
+
+rem /********** Pokemon Stadium 2 **********/
+rem [Mupen64Plus Options]
+for /f "delims=" %%a in ('sha256sum "Pokemon Stadium 2.n64"') do (
+    set "_pak_game_profiles[%%a]=Transfer pak"
+    set "_gfx_game_profiles[%%a]=mupen64plus-video-glide64mk2"
+    set "_emumode_game_profiles[%%a]=Dynamic Recompiler"
+    set "_params_game_profiles[%%a]="
 )

3bfe506fefd3dda9c602980b9c14a8ad74b12551
Giovanni Farfán B. <gfarfanb@gmail.com>
2025-11-13 13:21:11 -0600

    Setup JDK

---
+++ setup-jdk.bat
@@ -0,0 +1,53 @@
+@echo OFF
+set PWD=%cd%
+
+call env-vars.bat
+call require-var JDK_HOMES_FILE
+
+call source-file "%JDK_HOMES_FILE%"
+
+setlocal enableDelayedExpansion
+
+echo Select a JDK:
+for /l %%i in (1,1,%JDK_HOMES_LENGTH%) do (
+    set "_JDK_NAME=!JDK_NAMES[%%i]!"
+
+    for /f "delims=" %%j in ("!JDK_HOMES[%%i]!") do (
+        if "%%i"=="%JDK_DEFAULT_INDEX%" (
+            echo %%i^) ^(default^) !_JDK_NAME! [%%j]
+        ) else (
+            echo %%i^) !_JDK_NAME! [%%j]
+        )
+    )
+)
+
+set "_JDK_INDEX="
+set /P _JDK_INDEX="JDK-index> "
+if "%_JDK_INDEX%"=="" set _JDK_INDEX=%JDK_DEFAULT_INDEX%
+
+set "_JDK_NAME=!JDK_NAMES[%_JDK_INDEX%]!"
+
+if "%_JDK_NAME%"=="" (
+    echo Invalid JDK index
+    echo [Process stopped]: %0
+    goto back
+)
+
+set "JAVA_HOME=!JDK_HOMES[%_JDK_INDEX%]!"
+set "JAVA_JRE_HOME=!JRE_HOMES[%_JDK_INDEX%]!"
+
+endlocal & set "JAVA_HOME=%JAVA_HOME%" & set "JAVA_JRE_HOME=%JAVA_JRE_HOME%"
+
+echo Defined Environment Variables:
+echo     JAVA_HOME=%JAVA_HOME%
+echo     JAVA_JRE_HOME=%JAVA_JRE_HOME%
+goto completed
+
+:completed
+echo:
+echo [Completed]: %0
+goto back
+
+
+:back
+cd /d %PWD%

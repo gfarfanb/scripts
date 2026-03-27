0b5b5073d114adf36f801c34416d71d5d0b8e8bc
Giovanni Farfán B. <gfarfanb@gmail.com>
2026-03-27 14:12:52 -0600

    JD GUI

---
+++ dev/start-jd-gui.bat
@@ -0,0 +1,32 @@
+@echo OFF
+
+call env-vars
+call require-var JD_GUI_JAR
+
+goto main
+
+:usage
+echo Starts JD-GUI using binaries
+echo:
+echo Usage: %0 [^<jdk_version^>^|^<option^>]*
+echo Option:
+echo     -h: Displays this help message
+goto back
+
+:main
+if /i "%~1"=="-h" goto usage
+
+call setup-jdk %*
+
+"%JAVA_HOME%\bin\java" -jar "%JD_GUI_JAR%"
+goto completed
+
+
+:completed
+echo:
+echo [Completed]: %0
+goto back
+
+
+:back
+cd /d %PWD%
